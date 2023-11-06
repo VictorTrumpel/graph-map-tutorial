90314c69aeccf9493a6d089da0ffc552c03338a3
Victor Trumpel <victor.trumpel@gmail.com>
2023-11-06 16:00:17 +0300

    добавил валидацию имени при создании объекта на сцене

--- src/shared/HouseLabel/HouseLabel.tsx
+++ src/shared/HouseLabel/HouseLabel.tsx
@@ -1,6 +1,9 @@
 import { ChangeEvent, useState } from 'react';
-import { Button, Input, Form } from 'antd';
+import { Button, Input, Form, Spin } from 'antd';
 import { House } from '../House';
+import { IndexDB } from '@/IndexDB';
+import { useDebounce } from '../hooks/useDebounce';
+import cn from 'classnames';
 import './HouseLabel.css';
 
 type PropsType = {
@@ -10,27 +13,68 @@ type PropsType = {
 export const HouseLabel = ({ house }: PropsType) => {
   const [isMount, setIsMount] = useState(house.isMount);
 
-  const handleSaveHouse = () => {
+  const [error, setError] = useState<string | null>(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const [title, setTitle] = useState(house.name);
+
+  const handleSaveHouse = async () => {
+    const nameIsEmpty = !Boolean(title);
+
+    if (nameIsEmpty) {
+      setError('Имя не может быть пустым');
+      return;
+    }
+
+    if (error) return;
     house.mountHouse();
     setIsMount(true);
   };
 
+  const checkIsNameUniq = useDebounce(async (name: string) => {
+    const db = new IndexDB();
+
+    const houses = await db.getAllHousesInfo();
+
+    const nameIsTaken = houses.some(({ houseName }) => houseName === name);
+
+    if (nameIsTaken) {
+      setError('имя уже занято');
+      setLoading(false);
+      return;
+    }
+
+    setError(null);
+    setLoading(false);
+  }, 300);
+
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoading(true);
     const name = e.target.value;
+    setTitle(name);
     house.name = name;
+    checkIsNameUniq(name);
   };
 
   return (
     <Form className='house-label' onFinish={handleSaveHouse}>
-      <Input
-        disabled={isMount}
-        className='input-house-address'
-        defaultValue={house.name}
-        placeholder='Адрес'
-        onChange={handleNameChange}
-      />
+      <Form.Item className='input-house-address-container' rules={[{ required: true }]}>
+        <Input
+          disabled={isMount}
+          className={cn('input-house-address')}
+          placeholder='Адрес'
+          value={title}
+          status={error ? 'error' : ''}
+          onChange={handleNameChange}
+          suffix={loading ? <Spin size='small' /> : <></>}
+        />
+        {error && <div className='ant-form-item-explain-error'>{error}</div>}
+      </Form.Item>
+
       {!isMount && (
         <Button
+          disabled={loading || Boolean(error)}
           htmlType='submit'
           onSubmit={() => console.log('gdsgsd')}
           onPointerDown={handleSaveHouse}
